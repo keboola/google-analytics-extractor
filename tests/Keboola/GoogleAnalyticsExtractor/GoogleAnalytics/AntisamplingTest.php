66acd458d4678ad726d501804a05cc6127dc842a
Miroslav Cillik <miro@keboola.com>
2017-01-05 15:27:24 +0100

    chore (AntisamplingTest): DailyWalk extensive test

--- tests/Keboola/GoogleAnalyticsExtractor/GoogleAnalytics/AntisamplingTest.php
+++ tests/Keboola/GoogleAnalyticsExtractor/GoogleAnalytics/AntisamplingTest.php
@@ -8,12 +8,18 @@
 
 namespace Keboola\GoogleAnalyticsExtractor\GoogleAnalytics;
 
+use Keboola\GoogleAnalyticsExtractor\Extractor\Antisampling;
+use Keboola\GoogleAnalyticsExtractor\Extractor\Output;
+use Keboola\GoogleAnalyticsExtractor\Extractor\Paginator;
+use Symfony\Component\Filesystem\Filesystem;
+
 class AntisamplingTest extends ClientTest
 {
     private function buildQuery()
     {
         return [
             'name' => 'users',
+            'outputTable' => 'antisampling-test',
             'query' => [
                 'viewId' => getenv('VIEW_ID'),
                 'metrics' => [
@@ -35,16 +41,52 @@ class AntisamplingTest extends ClientTest
 
     public function testDailyWalk()
     {
+        $fs = new Filesystem();
+        if (!$fs->exists('/tmp/ga-test')) {
+            $fs->mkdir('/tmp/ga-test');
+        }
+        $fs->remove('/tmp/ga-test/*');
+
+        // Daily Walk
         $query = $this->buildQuery();
         $query['antisampling'] = 'dailyWalk';
         $query['samplingLevel'] = 'SMALL';
         $report = $this->client->getBatch($query);
 
-        $this->arrayHasKey($report['data']);
-        $this->assertNotEmpty($report['data']);
-        $this->arrayHasKey($report['query']);
-        $this->assertNotEmpty($report['query']);
-        $this->arrayHasKey($report['rowCount']);
+        $output = new Output('/tmp/ga-test', 'in.c-ex-google-analytics-test');
+        $paginator = new Paginator($output, $this->client);
+        $outputCsv = $output->createReport($query);
+        (new Antisampling($paginator, $outputCsv))->dailyWalk($query, $report);
+
+        $dailyWalkOutputCsv = $outputCsv->getPathname();
+
+        // Manual Daily Walk
+        $dates = [
+            date('Y-m-d', strtotime('-4 days')),
+            date('Y-m-d', strtotime('-3 days')),
+            date('Y-m-d', strtotime('-2 days')),
+            date('Y-m-d', strtotime('-1 days'))
+        ];
+
+        $query2 = $this->buildQuery();
+        $query2['outputTable'] = 'antisampling-expected';
+        $output = new Output('/tmp/ga-test', 'in.c-ex-google-analytics-test');
+        $paginator = new Paginator($output, $this->client);
+        $outputCsv = $output->createReport($query2);
+
+        foreach ($dates as $date) {
+            $query2['query']['dateRanges'] = [[
+                'startDate' => $date,
+                'endDate' => $date
+            ]];
+            $rep = $this->client->getBatch($query2);
+            $output->writeReport($outputCsv, $rep, $query2['query']['viewId']);
+            $paginator->paginate($query2, $rep, $outputCsv);
+        }
+
+        $expectedOutputCsv = $outputCsv->getPathname();
+
+        $this->assertFileEquals($expectedOutputCsv, $dailyWalkOutputCsv);
     }
 
     public function testAdaptive()
