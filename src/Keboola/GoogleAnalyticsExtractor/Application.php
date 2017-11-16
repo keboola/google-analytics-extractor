8c66258c45e0b5ec21aa7ba765809ecbb5a38911
Miroslav Čillík <miro@keboola.com>
2017-11-16 14:21:14 +0100

    feat: new google client with logging of retry requests

--- src/Keboola/GoogleAnalyticsExtractor/Application.php
+++ src/Keboola/GoogleAnalyticsExtractor/Application.php
@@ -43,12 +43,13 @@ class Application
             throw new UserException('Missing authorization data');
         }
         $tokenData = json_decode($config['authorization']['oauth_api']['credentials']['#data'], true);
-        $container['google_client'] = function () use ($config, $tokenData) {
+        $container['google_client'] = function ($c) use ($config, $tokenData) {
             return new RestApi(
                 $config['authorization']['oauth_api']['credentials']['appKey'],
                 $config['authorization']['oauth_api']['credentials']['#appSecret'],
                 $tokenData['access_token'],
-                $tokenData['refresh_token']
+                $tokenData['refresh_token'],
+                $c['logger']
             );
         };
         $container['google_analytics_client'] = function ($c) {
