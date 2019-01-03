98c12d17cae46920c9261b18f8a06eae6758b8f1
Vladimír Kriška <vlado@keboola.com>
2019-01-03 11:44:31 +0100

    Add also test with date dimension

--- tests/Keboola/GoogleAnalyticsExtractor/Extractor/OutputTest.php
+++ tests/Keboola/GoogleAnalyticsExtractor/Extractor/OutputTest.php
@@ -64,4 +64,49 @@ class OutputTest extends \PHPUnit_Framework_TestCase
         ], $output->createJsonSampleReport($query, $report));
     }
 
+    public function testCreateJsonSampleReportWithDateDimension()
+    {
+        $output = new Output('/tmp/output-test/json-sample-with-date-dim', 'output');
+        $query = [
+            'query' => [
+                'viewId' => 'profile-1',
+                'metrics' => [
+                    [
+                        'expression' => 'ga:exp1'
+                    ]
+                ],
+                'dimensions' => [
+                    [
+                        'name' => 'ga:dim1'
+                    ],
+                    [
+                        'name' => 'ga:date'
+                    ]
+                ],
+            ]
+        ];
+        $report = [
+            'data' => [
+                new Result(['ga:exp1' => 'met-val1'], ['ga:dim1' => 'dim-val1', 'ga:date' => '2019-01-01']),
+                new Result(['ga:exp1' => 'met-val2'], ['ga:dim1' => 'dim-val2', 'ga:date' => '2019-01-02']),
+            ]
+        ];
+
+        $this->assertEquals([
+            [
+                'id' => 'fa7617946a9db8f1cb76ce77813ee28f3d90696d',
+                'idProfile' => 'profile-1',
+                'exp1' => 'met-val1',
+                'dim1' => 'dim-val1',
+                'date' => '2019-01-01'
+            ],
+            [
+                'id' => '39865a11bfd0cd5f9f31d3b6d359f0108f700caa',
+                'idProfile' => 'profile-1',
+                'exp1' => 'met-val2',
+                'dim1' => 'dim-val2',
+                'date' => '2019-01-02'
+            ]
+        ], $output->createJsonSampleReport($query, $report));
+    }
 }
