2445c2fe869cf059ab54722e5a14de8a4b479ae5
Ondra Jodas <ondrej.jodas@keboola.com>
2022-03-02 21:58:47 +0100

    tests: run from last job date

---
+++ tests/functional/DatadirTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\GoogleAnalyticsExtractor\Functional;
+
+use Keboola\DatadirTests\DatadirTestCase;
+use Keboola\DatadirTests\DatadirTestSpecificationInterface;
+
+class DatadirTest extends DatadirTestCase
+{
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        $credentialsData = [
+            'access_token' => getenv('ACCESS_TOKEN'),
+            'refresh_token' => getenv('REFRESH_TOKEN'),
+        ];
+
+        putenv(sprintf(
+            'CREDENTIALS_DATA=%s',
+            json_encode($credentialsData)
+        ));
+
+        parent::__construct($name, $data, $dataName);
+    }
+}
