ac319db9ca2ff993d7bfdd1c0cef050a6986f5d0
Výborný Adam <mail@adamvyborny.cz>
2022-11-08 14:41:00 +0100

    Pagination

--- src/GoogleAnalytics/Client.php
+++ src/GoogleAnalytics/Client.php
@@ -57,14 +57,18 @@ class Client
 
     public function getAccountProperties(): array
     {
-        $response = $this->api->request(sprintf('%s?pageSize=200', self::ACCOUNT_PROPERTIES_URL));
-        $body = json_decode($response->getBody()->getContents(), true);
+        $accountSummaries = [];
 
-        if (isset($body['accountSummaries'])) {
-            return array_filter($body['accountSummaries'], fn(array $v) => isset($v['propertySummaries']));
-        }
+        do {
+            $response = $this->api->request(sprintf('%s?pageSize=200', self::ACCOUNT_PROPERTIES_URL));
+            $body = json_decode($response->getBody()->getContents(), true);
 
-        return [];
+            if (isset($body['accountSummaries'])) {
+                $accountSummaries[] = $body['accountSummaries'];
+            }
+        } while (isset($body['nextPageToken']));
+
+        return array_filter(array_merge([], ...$accountSummaries), fn(array $v) => isset($v['propertySummaries']));
     }
 
     public function getAccounts(): array
